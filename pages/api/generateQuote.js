d38f6538db545a8706b79b219a662aa99ffc4da2
longmene gibril <longmenegib@longmenegib.domain.local>
2023-04-21 21:19:32 +0100

    add headrers

--- pages/api/generateQuote.js
+++ pages/api/generateQuote.js
@@ -30,10 +30,10 @@ export const config = {
   };
 
 const handler = async (req, res) => {
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, authorization"
-    );
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); 
     //Preflight CORS handler
     if (req.method === "OPTIONS") {
         return res.status(200).json({
